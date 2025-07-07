be63ca18251e239d730140062b98189a00c11365
Tisha7353 <tgtishagarg@gmail.com@gmail.com>
2025-07-07 20:55:18 +0530

    temp

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -22,7 +22,8 @@ interface ChatStore {
 	setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" 
+		: "https://resono.onrender.com"
 
 const socket = io(baseURL, {
 	autoConnect: false, // only connect if user is authenticated
