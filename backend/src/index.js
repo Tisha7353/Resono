82c10b10ba161945a308de64e1889e6da393e0c8
Tisha7353 <tgtishagarg@gmail.com@gmail.com>
2025-07-07 19:48:54 +0530

    temp

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ initializeSocket(httpServer);
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://resono.vercel.app/",
 		credentials: true,
 	})
 );
@@ -72,12 +72,7 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("/", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
-}
+
 
 
 
