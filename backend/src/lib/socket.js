393db64d94c3a6a2c84ca28273b38febc9ff6f66
Tisha <127007536+Tisha7353@users.noreply.github.com>
2025-07-10 11:55:14 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,7 @@ import { Message } from "../models/message.model.js";
 export const initializeSocket = (server) => {
 	const io = new Server(server, {
 		cors: {
-			origin: "https://resono.vercel.app/",
+			origin: "https://resono.vercel.app",
 			credentials: true,
 		},
 	});
