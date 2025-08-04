9234269476b5142bd0fcedfc8e7b817cc98e3924
Tisha7353 <tgtishagarg@gmail.com>
2025-08-04 18:17:22 +0530

    Fixed topbar

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -5,18 +5,14 @@ import SignInOAuthButtons from "./SignInOAuthButtons"
 import { useAuthStore } from "@/stores/useAuthStore";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "./ui/button";
-import { useState } from "react";
-import { Input } from "./ui/input";
+
+
 
 const TopBar = () => {
 	const { isAdmin } = useAuthStore();
 	console.log({ isAdmin });
-const [searchQuery, setSearchQuery] = useState("");
 
-	const handleSearch = (e:any) => {
-		e.preventDefault();
-		console.log("Search:", searchQuery); // Handle the search logic here
-	};
+
 	return (
 		<div
 			className='flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 
@@ -29,16 +25,7 @@ const [searchQuery, setSearchQuery] = useState("");
 			</div>
 			
 			<div className='flex items-center  gap-4'>
-				{/* Search Bar */}
-			<form onSubmit={handleSearch} className='flex-1 max-w-56 mx-4'>
-				<Input
-							  type='text'
-							  value={searchQuery}
-							  onChange={(e) => setSearchQuery(e.target.value)}
-							  placeholder='Search songs...'
-							  className='border border-gray-200 text-gray-200 w-full md:max-w-xs'
-							/>
-			</form>
+			
 				{isAdmin && (
 					<Link to={"/admin"} className={cn(buttonVariants({ variant: "outline" }))}>
 						<LayoutDashboardIcon className='size-4  mr-2' />
